202012d81d9a708e5baf929279e4137056e06bf8
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2026-01-12 21:35:47 -0800

    Add Google Tag Manager plugin and update Google Analytics tracking ID

--- source/gatsby-config.ts
+++ source/gatsby-config.ts
@@ -12,14 +12,42 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-postcss",
     {
+      // https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
       resolve: "gatsby-plugin-google-gtag",
       options: {
         trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
+          "G-HCVZJ6MMET", // Google Analytics / GA
         ],
-        pluginConfig: {
-          head: true,
-        },
+        pluginConfig: {},
+      },
+    },
+    {
+      // https://www.gatsbyjs.com/plugins/gatsby-plugin-google-tagmanager/
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MM8XL2ZX",
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+        // Defaults to false
+        enableWebVitalsTracking: true,
       },
     },
     "gatsby-plugin-image",
